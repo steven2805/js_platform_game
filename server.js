fb94347ced5447d18c5eaaece8d429c49527c957
Glen Young <extractofcactus@icloud.com>
2017-08-11 11:04:55 +0100

    server running; blank page showing

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var parser = require('body-parser');
+var gameApp = express();
+// var path = require('path'); 
+
+
+gameApp.use(parser.json());
+gameApp.use(parser.urlencoded({extended: true}));
+gameApp.use(express.static('client/build'));
+
+gameApp.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/build/index.html');
+});
+
+
+
+var server = gameApp.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("gameApp listening at", host, port);
+});
