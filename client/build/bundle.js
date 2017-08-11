c02634340dfb907b4687f8f81e7ac55150ff4722
Glen Young <extractofcactus@icloud.com>
2017-08-11 14:54:04 +0100

    level coords mapped

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -105,23 +105,20 @@ module.exports = levelTest;
 var Level = function(plan) {
   this.width = plan[0].length;
   this.height = plan.length;
-  this.startPoint = [0, 0];
-  var map = [[0, 0]];
+  var startPoint = [0, 0];
+  var map = [];
 
   var yArray = [];
   var xArray = [];
 
   for (var y = 0; y < this.height; y++) {
-    var yCoord = this.startPoint[1] += 80;
+    var yCoord = y * 80;
     yArray.push(yCoord);
-    for (var x = 0; x < this.height; x++) {
-      var xCoord = this.startPoint[1] += 80;
-      xArray.push(xCoord);
-    }
   }
-
-
-
+  for (var x = 0; x < this.width; x++) {
+    var xCoord =  x * 80;
+    xArray.push(xCoord);
+  }
   for (var yElement of yArray) {
     for (var xElement of xArray) {
       var coord = [xElement, yElement]
@@ -129,17 +126,9 @@ var Level = function(plan) {
     }
   }
 
-  // for (var y = 0; y < this.height; y++) {
-  //   this.startPoint[1] += 80;
-  //   for (var x = 0; x < this.width; x++) {
-  //     this.startPoint[0] += 80;
-  //     this.map.push(this.startPoint);
-  //   }
-  //   this.map.push(this.startPoint);
-  //   this.startPoint[0] = 0; 
-  // }
-
-   console.log(map);
+  console.log(yArray);
+  console.log(xArray);
+  console.log(map);
 }
 
 module.exports = Level;
