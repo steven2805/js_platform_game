36e494eaa1bce949cbbbc507476316810fbc80c7
Glen Young <extractofcactus@icloud.com>
2017-08-12 14:34:26 +0100

    player can move left and right

--- client/src/game_logic/game.js
+++ client/src/game_logic/game.js
@@ -2,17 +2,58 @@ var levelTest = require('./levels');
 var Level = require('./level_constructor');
 var Player = require('./player');
 
+var leftKeyPressed = false;
+var rightKeyPressed = false;
+
+var keyDownHandler = function(evt) {
+  if (evt.keyCode === 77) {
+    rightKeyPressed = true;
+  }
+  else if (evt.keyCode === 78) {
+    leftKeyPressed = true;
+  }
+} 
+
+var keyUpHandler = function(evt) {
+  if (evt.keyCode === 77) {
+    rightKeyPressed = false;
+  }
+  else if (evt.keyCode === 78) {
+    leftKeyPressed = false;
+  }
+}
+
+// var drawPlayer = function 
 
 
 var gameApp = function() {
   var levelOne = new Level(levelTest);
   levelOne.setUpMap();
   var player = new Player(levelOne.playerStart);
-  player.draw(levelOne.playerStart);
+  player.draw([levelOne.playerStart[0], levelOne.playerStart[1] + 40]);
 
-  console.log(player.startingCoords);
+  setInterval(function() {
+  var oldCoords = player.position;
+  var newCoords = oldCoords;
 
-  window.addEventListener('keydown', );
+  document.addEventListener('keydown', keyDownHandler, false)
+  document.addEventListener('keyup', keyUpHandler, false)
+
+    if (rightKeyPressed) {
+      newCoords = [oldCoords[0] + 10, oldCoords[1]];
+      player.draw(newCoords);
+      oldCoords = newCoords;
+    }
+    if (leftKeyPressed) {
+      newCoords = [oldCoords[0] - 10, oldCoords[1]];
+      player.draw(newCoords);
+      oldCoords = newCoords;
+    }
+  }, 10)
+ 
+  
+  
+  
 }
 
 window.addEventListener('load', gameApp);
--- client/src/game_logic/player.js
+++ client/src/game_logic/player.js
@@ -1,16 +1,19 @@
 
 
 
-var Player = function(startingCoords) {
-  this.startingCoords = startingCoords;
+var Player = function(position) {
+  this.position = position;
 }
 
 Player.prototype.draw = function(coords) {
   var canvas = document.getElementById("game-canvas");
   var context = canvas.getContext("2d");
+
+  context.clearRect(this.position[0], this.position[1], 20, 40);
   context.fillStyle = 'red';
   context.fillRect(coords[0], coords[1], 20, 40);
   context.stroke();
+  this.position = coords;
 };
 
 module.exports = Player;
